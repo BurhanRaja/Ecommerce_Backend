660752642f81577f2c6eab5de9b7bc7c45eac70b
BurhanuddinRaja <mails4burhan@gmail.com>
2023-01-27 15:19:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,87 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+// TODO: 
+
+// Database Tables:-
+
+// Products
+// Categories
+// Sub-Categories
+// User
+// Seller/Company
+// Orders
+
+
+// Client API
+
+// On Every Purchase send a notification
+
+// Authentication
+// Login
+// Register
+
+// Get User Related Details
+// Get User Profile
+// Get the Past Purchase
+
+// Fashion and Electronics
+// Get All Items
+// Get Filtered Items based on sub-category like bags, watches, men, women
+// Get Filtered Items based on Price
+// Get Top Products
+// Get Featured Products
+// Get Individual Item Card, Reviews, Description
+
+// Categories
+// Get All Categories
+// Get sub-category of a particular Category
+
+// Cart
+// Post/Add product to cart
+// Get the Products for Cart
+// Get the calculated amount by adding gst and shipping charges
+
+// Buy Now
+// Flow :- Add to Cart, Redirect to Checkout
+// Get Existing Address if yes OR display a form to add address.
+// Payment Integration
+
+// After Payment
+// POST/Add the order to the Order table.
+
+
+
+
+// Seller API
+
+// Authentication
+// Register a Brand or Company
+// Login
+
+// Products
+
+// CRUD on Products
+// CRUD on Sub-categories
+
+// Orders
+
+// Get All Orders that you get
+// POST/Add to order (Delivered or Pending)
+
+// Earnings
+
+// Get Payment after client Pay
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
