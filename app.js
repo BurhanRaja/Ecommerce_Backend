c377873c88f825d8fcce1a45635bb056acd1eed8
BurhanRaja <burhanraja02@yahoo.com>
2023-05-15 22:12:52 +0530

    local user test added

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { PORT } = require("./config/config");
+const connectToMongoDB = require("./db");
+dotenv.config();
+
+connectToMongoDB();
+
+const app = express();
+app.use(express.static("public"));
+
+const port = PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/client", require("./routes/client/index")); // Client or User
+app.use("/api/user/address", require("./routes/useraddress/index")); // User Address
+
+app.use("/api/seller", require("./routes/seller/index")); // Admin or company or seller
+app.use("/api/seller/info", require("./routes/sellerinfo/index")); // Seller Info
+
+app.use("/api/parentcategory", require("./routes/parentcategory/index")); // Parent Category
+app.use("/api/category", require("./routes/category/index")); // Category
+app.use("/api/subcategory", require("./routes/subcategory/index")); // Subcategory
+
+app.use("/api/product", require("./routes/product/index")); // Products
+
+app.use("/api/discount", require("./routes/discount/index")); // Discount
+
+app.use("/api/cart", require("./routes/cart/index")); // Cart
+
+app.use("/api/order", require("./routes/order/index")); // Order
+
+app.use("/api/sellerorder", require("./routes/sellerorder/index")); // Seller Order
+
+app.use("/api/payments", require("./routes/payment/index")); // Payment
+
+
+
+module.exports = app;
